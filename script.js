43a9509264163ae416ff8c64401128735d3840cc
PRINCE-PRASAD <princeprasad.code@gmail.com>
2022-03-13 21:31:21 +0530

    version 2.0

--- script.js
+++ script.js
@@ -6,6 +6,11 @@ const title = document.querySelector("#title");
 const prev = document.getElementById("prev");
 const next = document.getElementById("next");
 
+let progress = document.getElementById("progress");
+let total_duration = document.getElementById("duration");
+let current_time = document.getElementById("current_time");
+const progress_div = document.getElementById('progress_div');
+
 const song_data = [
     {
         name: "fav-1",
@@ -88,5 +93,67 @@ const prevSong = () => {
     playSong();
 };
 
+// progress js portion
+
+song.addEventListener('timeupdate', (event) => {
+    // console.log(event);
+    const { currentTime, duration } = event.srcElement;
+    // console.log(currentTime);
+    // console.log(duration);
+
+    let progress_time = (currentTime / duration) * 100;
+    progress.style.width = `${progress_time}%`;
+    // console.log(progress_time);
+
+    // music duration update
+
+
+    // console.log(duration);
+
+    let minutes_duration = Math.floor(duration / 60); /*math.floor is the functin where we get number in round figure without any decimal*/
+    let second_duration = Math.floor(duration % 60);
+
+    // console.log(minutes_duration);
+    // console.log(second_duration);
+
+    let tot_duration = `${minutes_duration}:${second_duration}`;
+    if (duration) { /*for removing nana while changing the music */
+        total_duration.textContent = `${tot_duration}`;
+    }
+
+    //current duration update
+
+    let minutes_currentTime = Math.floor(currentTime / 60);
+    let second_currentTime = Math.floor(currentTime % 60);
+
+    if(second_currentTime < 10) {
+        second_currentTime = `0${second_currentTime}`;/*for adding 0 in the current_time */
+    }
+    
+    let tot_currentTime = `${minutes_currentTime}:${second_currentTime}`;
+    
+        current_time.textContent = `${tot_currentTime}`;
+    
+});
+
+//for progress onclick function
+
+progress_div.addEventListener("click", (event) => {
+    console.log(event);
+    const { duration } = song;
+
+    let move_progress = 
+    (event.offsetX / event.srcElement.clientWidth) * duration; /* here clintwidth it show how may time present in sec in song and offsetx show where u click  */
+    
+    console.log(duration);
+    console.log(move_progress);
+
+    song.currentTime = move_progress;
+});
+
+// for automatic next song function
+song.addEventListener("ended", nextsong);
+
+
 next.addEventListener("click", nextsong);
-prev.addEventListener("click", prevSong);
+prev.addEventListener("click", prevSong);  
